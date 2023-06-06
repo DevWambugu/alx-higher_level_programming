999004c40fb75df80930287ec0366532df2ccd0c
DevWambugu <venusdata100@gmail.com>
2023-06-06 13:06:56 -0700

    add abs inbuilt function

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,6 +3,6 @@
 #  print the last digit of a number
 def print_last_digit(number):
     ''' prints the last digit of a number.'''
-    last_digit = number % 10
-    print (last_digit, end="")
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
     return last_digit
