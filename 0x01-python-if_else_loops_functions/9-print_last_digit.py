5cc608076b8eb6d4dbbc821ccf7e0b11846c1127
DevWambugu <venusdata100@gmail.com>
2023-06-06 13:03:46 -0700

    debug 6

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,4 +4,5 @@
 def print_last_digit(number):
     ''' prints the last digit of a number.'''
     last_digit = number % 10
-    print (last_digit)
+    print (last_digit, end="")
+    return last_digit
