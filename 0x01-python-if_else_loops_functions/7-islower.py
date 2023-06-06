476f476644632957f0bf367ab63e6026894194bd
DevWambugu <venusdata100@gmail.com>
2023-06-06 12:00:12 -0700

    pycode style documentation

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#checks for lowercase character
+#  checks for lowercase character
 def islower(c):
     '''Write a function that checks for lowercase character.'''
     if ord(c) >= 97:
