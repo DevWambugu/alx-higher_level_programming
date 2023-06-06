d03857e5ac33aa4c3f215acf4abef5a21c7fa417
DevWambugu <venusdata100@gmail.com>
2023-06-06 12:18:34 -0700

    convert to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# Print string in uppercase
+def uppercase(str):
+    '''Prints a string in uppercase followed by a new line'''
+    for i in str:
+        if ord(i) >= 97:
+            A_letter = ord(i) - 32
+            letter = chr(A_letter)
+            print("{}".format(letter))
+        else:
+            print("{}".format(i))
