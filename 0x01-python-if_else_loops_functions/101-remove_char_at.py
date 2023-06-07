e1275c46acbd86cfea901dd5f68258896fc972af
DevWambugu <venusdata100@gmail.com>
2023-06-07 15:03:47 -0700

    use the c array way not the python way

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -4,5 +4,9 @@
 # remove the characters at a specific position
 def remove_char_at(str, n):
     '''copies the string and removes a character at a specific location'''
-    new_str = str[:n] + str[n+1:]
-    return new_str
+    if n < 0:
+        return str
+    else:
+        new_str = str[:n] + str[n+1:]
+        return new_str
+
