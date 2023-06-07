a71e9291aef70f2f44a3d8c2512b7edd60c46cca
DevWambugu <venusdata100@gmail.com>
2023-06-07 14:32:44 -0700

    change syntax to remove the extra condition

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,9 +6,6 @@ def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
             A_letter = ord(i) - 32
-            letter = chr(A_letter)
-            print("{}".format(letter), end="")
-        else:
-            print("{}".format(i), end="")
-
+            i = chr(A_letter)
+        print("{}".format(i), end="")
     print("")
