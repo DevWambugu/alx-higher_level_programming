f37a007d5c4a4178d91f70f1d2b2c2a3c16e0ca3
DevWambugu <venusdata100@gmail.com>
2023-06-08 11:28:13 -0700

    add string literal

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,6 +2,7 @@
 #  import the function def add(a, b): from the file add_0.py
 #  print the result of the addition 1 + 2 = 3
 if __name__ == "__main__":
+    '''prints the sum of 2 integers'''
     from add_0 import add
     a = 1
     b = 2
