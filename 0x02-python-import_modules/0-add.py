94f9abbdc512f3c3bade64d9341793a4837213cb
DevWambugu <venusdata100@gmail.com>
2023-06-08 11:24:54 -0700

    ensure code is not run at import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 #  import the function def add(a, b): from the file add_0.py
 #  print the result of the addition 1 + 2 = 3
-
-from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
