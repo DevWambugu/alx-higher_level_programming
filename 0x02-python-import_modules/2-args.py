f35cc89a7a696886672fa825c5e408c10ae779a5
DevWambugu <venusdata100@gmail.com>
2023-06-08 13:15:30 -0700

    print a list of arguments trial 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     args = len(sys.argv)
     # print number of arguments and the args
     if args == 2:
-        print("{} argument:".format(args))
-        print(sys.argv[0])
+        print("{} argument:".format(args - 1))
+        print(sys.argv[1])
     elif args == 1:
         print("{} arguments.".format(0))
     else:
-        print("{} arguments:".format(args))
+        print("{} arguments:".format(args - 1))
         for i in range(1, args):
             print("{}: {}".format(i, sys.argv[i]))
