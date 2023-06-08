723141a0244822bc425f939cf86f261bab2bf635
DevWambugu <venusdata100@gmail.com>
2023-06-08 12:25:01 -0700

    do some math

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#  import functions from the file calculator_1.py
+#  import functions from the file
 #  do some Maths, and prints the result
 
 if __name__ == "__main__":
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
+    
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
