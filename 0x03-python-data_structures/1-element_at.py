0244ab6ca6227b55071f148f74a77da581ed9740
DevWambugu <venusdata100@gmail.com>
2023-06-11 12:46:18 -0700

    debug 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,7 +5,7 @@ def element_at(my_list, idx):
     '''retrieves an element from a list like c'''
     if idx < 0:
         return (0)
-    elif idx > len(my_list):
+    elif idx > (len(my_list) - 1):
         return (0)
     else:
         return (my_list[idx])
