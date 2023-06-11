7fa9244713596fa5ee556a0a2a7fac03fa5d036e
DevWambugu <venusdata100@gmail.com>
2023-06-11 12:31:07 -0700

    print intgers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+#  prints all integers of a list
+def print_list_integer(my_list=[]):
+    '''prints all integers in a given list'''
+    for i in my_list:
+        print("{}".format(i))
