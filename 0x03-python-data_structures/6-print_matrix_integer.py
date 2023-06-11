14db3dd3a75d1d6efb0bc7288c5b3803aa1f71e6
DevWambugu <venusdata100@gmail.com>
2023-06-11 14:18:06 -0700

    debug 3

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,9 @@
 # print a matrix of integers
 def print_matrix_integer(matrix=[[]]):
     '''print a matrix of integers.'''
-    for r in matrix:
-        for el in r:
-            print("{:d}".format(el), end=" ")
+    for r in range(len(matrix)):
+        for j in range(len(matrix[r])):
+                print("{:d}".format(matrix[r][j]), end="")
+                if j != (len(matrix[r]) - 1):
+                    print(" ", end="")
         print("{}".format(""))
