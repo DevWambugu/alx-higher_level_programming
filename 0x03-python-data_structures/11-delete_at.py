2d3b99330e30b4b121c87684e3fd44c60019c527
DevWambugu <venusdata100@gmail.com>
2023-06-12 02:30:29 -0700

    delete an element

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#  delete an item at a specif location
+def delete_at(my_list=[], idx=0):
+    '''deletes an item at a specific index'''
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    del my_list[idx]
+    return my_list
