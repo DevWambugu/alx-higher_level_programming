b8cd4df65ecf7eeb7cbd011a83c3337dc3e61ef8
DevWambugu <venusdata100@gmail.com>
2023-06-14 05:45:56 -0700

    replace certain elements

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+#  replace all occurrences of an element by another in a new list
+def search_replace(my_list, search, replace):
+    '''replaces all occurrences of an element by another in a new list'''
+    new_list = list(map(lambda x: x if x != search else replace, my_list))
+    return new_list
