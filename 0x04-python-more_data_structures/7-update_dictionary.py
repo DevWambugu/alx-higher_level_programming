ec0ce0dfbeec0ee944b267dd2c96da2195993a22
DevWambugu <venusdata100@gmail.com>
2023-06-14 07:40:00 -0700

    replace or add key

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+#  replace or adds key/value in a dictionary
+def update_dictionary(a_dictionary, key, value):
+    '''replaces or adds key/value in a dictionary'''
+    a_dictionary[key] = value
+    return a_dictionary
