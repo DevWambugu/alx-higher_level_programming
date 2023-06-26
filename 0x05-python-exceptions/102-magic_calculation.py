2dc55b58c238bc9a1bb86d61aace60188835f29e
DevWambugu <venusdata100@gmail.com>
2023-06-26 14:35:49 -0700

    reverse the bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#  does magical calculations
+def magic_calculation(a, b):
+    '''returns (a / b) ** i'''
+    rst = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            rst += (a / b) ** i
+        except:
+            rst += a + b
+    return rst
