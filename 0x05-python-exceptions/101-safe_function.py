390c93cee03a97689087359b68e382737045a5fa
DevWambugu <venusdata100@gmail.com>
2023-06-26 12:23:14 -0700

    executes a function safely.

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+#  executes a function safely
+def safe_function(fct, *args):
+    '''executes a function safely
+    fct will be always a pointer to a function
+    Returns the result of the function'''
+    try:
+        result = fct(*args)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        result = None
+    return result
