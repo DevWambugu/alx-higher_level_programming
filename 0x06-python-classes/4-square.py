3af1f019996a47d40ea77d35b6c039de70b271ce
DevWambugu <venusdata100@gmail.com>
2023-06-27 11:05:18 -0700

    assign value to private instance size

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -33,7 +33,8 @@ class Square:
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+i            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """Public instance method
