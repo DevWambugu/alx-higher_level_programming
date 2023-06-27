42520d0eba811dc7b87274fd351af2b28b748c37
DevWambugu <venusdata100@gmail.com>
2023-06-27 10:34:12 -0700

    add an instance tat returns the area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# 3-square.py
+# DevWambugu
+"""Define a class Square."""
+
+
+class Square:
+    '''Define a class Square'''
+    def __init__(self, size=0):
+        """Instantiation with optional size.
+        size must be an integer, otherwise raise a TypeError
+        exception with the message 'size must be an integer'.
+        If size is less than 0, raise a ValueError exception
+        with the message 'size must be >= 0'.
+
+        Args:
+            size (int): The type and value of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Public instance method
+        that returns the current square area.
+        """
+        area = self.__size * self.__size
+        return area
