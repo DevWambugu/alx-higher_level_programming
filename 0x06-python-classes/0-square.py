e387bbaa252f4c612021d84a52a453be2884780e
DevWambugu <venusdata100@gmail.com>
2023-06-27 09:34:37 -0700

    add docs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,8 @@
 
 #  define an empty class called square
 class Square:
-    '''define an empty class called square'''
+    '''This class defines an empty class
+    called square. No module should be imported.
+    The are no attributes or methods defined yet
+    '''
     pass
