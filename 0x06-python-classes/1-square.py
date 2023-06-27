f104489deff857e72c44c29e4f2fa3794d021e79
DevWambugu <venusdata100@gmail.com>
2023-06-27 09:56:34 -0700

    create a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 1-square.py
+# DevWambugu
+"""Define a class Square."""
+
+
+class Square:
+    '''Define a class Square
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    '''
+    def __init__(self, size)
+    self.__size = size
