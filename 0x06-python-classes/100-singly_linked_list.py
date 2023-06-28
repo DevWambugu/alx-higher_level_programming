9590cd398926f2e0feca38d641cba4c1335033a6
DevWambugu <venusdata100@gmail.com>
2023-06-28 05:31:37 -0700

    singly linked list

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+# 2-square.py
+# DevWambugu
+"""Define a class Node."""
+
+
+class Node:
+    '''defines a class node'''
+
+    def __init__(self, data, next_node=None):
+        '''set a private attribute called data'''
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        '''retrieve the data attribute'''
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        '''sets the attribute'''
+        if isinstance(value, int):
+            self.__data = value
+        else:
+            raise TypeError("data must be an integer")
+
+    @property
+    def next_node(self):
+        '''retrieve attribute'''
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        '''set the attribute'''
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        '''Private instance attribute: head (no setter or getter)'''
+        self.__head = None
+
+    def sorted_insert(self, value):
+        '''inserts a new Node into the correct sorted position
+        in the list (increasing order)
+        '''
+        new_node = Node(value)
+        if self.__head is None:
+            self.__head = new_node
+        elif value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+        else:
+            current = self.__head
+            while current.next_node is not None and value > current.next_node.data:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        '''Return a string representation of the linked list'''
+        current = self.__head
+        nodes = []
+        while current is not None:
+            nodes.append(str(current.data))
+            current = current.next_node
+        return '\n'.join(nodes)
