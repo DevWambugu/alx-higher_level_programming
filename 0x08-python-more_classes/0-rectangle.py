f9fdd2453d358b02f212e9e15a05d89cda63198b
DevWambugu <venusdata100@gmail.com>
2023-07-03 02:30:51 -0700

    create an empty class called rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# DevWambugu
+"""Define a class rectangle."""
+
+
+class Rectangle:
+    """Define a class rectangle
+    the attributes for this class have
+    not been defined"""
+
+    pass
