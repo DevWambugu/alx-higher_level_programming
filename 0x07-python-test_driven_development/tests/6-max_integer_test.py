989a99968a82cf7e79170952ca370121c1cde0c8
DevWambugu <venusdata100@gmail.com>
2023-07-07 18:34:53 -0700

    test for ordered list

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,9 +5,12 @@ max_integer = __import__('6-max_integer').max_integer
 
 class max_integer_test(unittest.TestCase):
     
-    def max_integer(self):
-        '''Searches for max int in  an ordered list'''
+    def test_max_integer(self):
         lst = [1, 3, 5, 2, 4]
-        self.assertEqual(max_integer(ordered), 5)
+        self.assertEqual(max_integer(lst), 5)
+
+    def test_max_integer_ordered(self):
+        lst = [1, 2, 3, 4, 5]
+        self.assertEqual(max_integer(lst), 5)
 if __name__ == '__main__':
     unittest.main()
