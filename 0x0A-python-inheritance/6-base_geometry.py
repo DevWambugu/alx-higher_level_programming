3a19b6053e7c5ed7c8de85035db719acee6e8e7d
DevWambugu <venusdata100@gmail.com>
2023-07-10 11:23:09 -0700

    raises an Exception error

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#  6-base_geometry.py
+#  DevWambugu
+"""improve the class to raise an exception error"""
+
+
+class BaseGeometry:
+    '''This class containdsa function that raises an exception error'''
+    def area(self):
+        '''this function raises an exception error'''
+        raise Exception("area() is not implemented")
