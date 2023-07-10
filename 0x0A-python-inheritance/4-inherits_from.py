f7fe36eb0711b18f226bcd9e7ffa8811a7b226a8
DevWambugu <venusdata100@gmail.com>
2023-07-10 08:50:40 -0700

    pycode style

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,9 +5,9 @@
    (directly or indirectly) from the specified class."""
 
 
-   def inherits_from(obj, a_class):
-       '''this function returns True if the object is an instance of a
-       class that inherited (directly or indirectly) 
-       from the specified class ; otherwise False
-       '''
-       return isinstance(obj, a_class) and type(obj) != a_class
+def inherits_from(obj, a_class):
+    '''this function returns True if the object is an instance of a
+    class that inherited (directly or indirectly)
+    from the specified class ; otherwise False
+    '''
+    return isinstance(obj, a_class) and type(obj) != a_class
