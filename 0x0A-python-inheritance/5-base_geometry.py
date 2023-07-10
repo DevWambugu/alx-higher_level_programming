c89ec35d1bd01f7166b66fbd0b4bc58396e375ff
DevWambugu <venusdata100@gmail.com>
2023-07-10 11:05:18 -0700

    create an empty class called BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#  5-base_geometry.py
+#  DevWambugu
+"""create an empty class Base Geometry"""
+
+
+class BaseGeometry:
+    '''This is an empty class called BaseGeometry'''
+    pass
