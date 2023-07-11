b7b07a0c28b93cfd37348f3e68e92c2922a8d1ad
DevWambugu <venusdata100@gmail.com>
2023-07-11 11:41:02 -0700

    check if attribute contains a list of strings

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#  9-student.py
+#  DevWambugu
+'''a class that defines the class student with public attributes'''
+
+
+class Student:
+    '''This class defines the class student'''
+    def __init__(self, first_name, last_name, age):
+        '''initializes the public attributes'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''This function retrieves a dictionary
+        representation of a Student instance'''
+        if attrs is None:
+            return self.__dict__
+        else:
+            if all(isinstance(attr, str) for attr in attrs):
+                return {attr: getattr(self, attr)
+                        for attr in attrs if hasattr(self, attr)}
+            else:
+                return self.__dict__
