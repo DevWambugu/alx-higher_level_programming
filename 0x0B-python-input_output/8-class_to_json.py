1b567174adc4bf621058eea4c4ea36eae49f4336
DevWambugu <venusdata100@gmail.com>
2023-07-11 11:04:37 -0700

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+#  8-class_to_json.py
+#  DevWambugu
+''' returns the dictionary description
+with simple data structure
+(list, dictionary, string, integer and boolean)
+for JSON serialization of an object
+'''
+import json
+
+
+def class_to_json(obj):
+    '''this function takes an object obj
+    and returns a dictionary description
+    with a simple data structure'''
+    return obj.__dict__
