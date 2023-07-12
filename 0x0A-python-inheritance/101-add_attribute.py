2b2d9ac91b8e6ddbe97ec5102ed1dc740d5f87a4
DevWambugu <venusdata100@gmail.com>
2023-07-12 16:08:24 -0700

    set attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#   101-add_attribute.py
+#   DevWambugu
+'''Write a function that adds a new attribute
+to an object if it’s possible'''
+
+
+def add_attribute(obj, attr_name, attr_value):
+    '''adds a new attribute. if not possible
+    it raises a type error'''
+    if hasattr(obj, attr_name):
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, attr_name, attr_value)
