4d90e7209f06bdde504184bc73ac520de3783520
DevWambugu <venusdata100@gmail.com>
2023-07-12 16:01:01 -0700

    reverse functions

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#  100-my_int.py
+#  DevWambugu
+'''this class MYint inherits from int'''
+
+
+class MyInt(int):
+    '''my class that inherits from the class int'''
+    def __eq__(self, other):
+        '''This function overrides the equals
+        method'''
+        return not super().__eq__(other)
+
+    def __ne__(self, other):
+        '''This function overides the not equals
+        method'''
+        return super().__eq__(other)
