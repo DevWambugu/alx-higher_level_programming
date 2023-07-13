8d8e7f55874c7eda7f242f8ad1920d438a160a7e
DevWambugu <venusdata100@gmail.com>
2023-07-13 12:24:13 -0700

    print the rectangle instance

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,9 @@ class Rectangle(Base):
     def area(self):
         '''returns the area of the rectangle'''
         return self.__width * self.__height
+
+    def display(self):
+        ''' prints in stdout the Rectangle
+        instance with the character #'''
+        for i in range(self.__height):
+            print("#" * self.__width)
