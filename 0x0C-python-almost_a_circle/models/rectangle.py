12020db5072d86d7b8144f2e39af090fc583d620
DevWambugu <venusdata100@gmail.com>
2023-07-19 11:45:15 -0700

    trial 7 git push "
    "
    "

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-#  base.py
+#  rectangle.py
 #  DevWambugu
 '''This class is be the “base” of all other classes in this project.
    The goal of it is to manage id attribute in all your future classes
