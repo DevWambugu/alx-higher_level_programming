1ba06fb4c301cd1d8df507babd0bdc3c8b8e71ab
DevWambugu <venusdata100@gmail.com>
2023-09-12 15:01:36 +0000

    number of occcurences in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let numberOfOccurences = 0;
+
+  for (const element of list) {
+    if (element === searchElement) {
+      numberOfOccurences++;
+    }
+  }
+  return (numberOfOccurences);
+};
