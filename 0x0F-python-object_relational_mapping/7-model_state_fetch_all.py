1794b36f36b7282e8dbe4dc45be65cdc025f2266
DevWambugu <venusdata100@gmail.com>
2023-09-17 01:54:05 +0000

    debug 4

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-lists all State objects from the database hbtn_0e_6_usa
+This scriptlists all State objects
+from the database hbtn_0e_6_usa
 '''
 
 import sys
@@ -18,8 +19,11 @@ if __name__ == '__main__':
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
+
     session = Session()
+
     states = session.query(State).order_by(State.id.asc()).all()
+
     for state in states:
         print(f"{state.id}:", state.name)
 session.close()
