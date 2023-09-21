c62980ffa03360eb37d0797258c31fc2e766fce6
DevWambugu <venusdata100@gmail.com>
2023-09-21 23:25:16 +0000

    task 101 pycodestyle

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,10 +16,10 @@ def main():
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
     for city in state.cities:
