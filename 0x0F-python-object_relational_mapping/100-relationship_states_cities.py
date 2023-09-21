650465038f00f3bf88d39f69e11b5cee3da122e2
DevWambugu <venusdata100@gmail.com>
2023-09-21 21:23:50 +0000

    create a state and city

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+from sys import argv
+
+
+def main():
+    """creates the State “California” with the City
+    “San Francisco” from the database hbtn_0e_100_usa"""
+    password = argv[2]
+    username = argv[1]
+    database = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, database))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    main()
