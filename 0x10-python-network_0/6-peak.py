e1e1702c61fb555048858efc1421bb45ff129f1d
DevWambugu <venusdata100@gmail.com>
2023-09-28 12:12:22 +0000

    first advance task

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,9 +8,10 @@ def find_peak(list_of_integers):
     '''find the peak in a list of integers'''
     if len(list_of_integers) < 3:
         return None
-    
+
     for list_index in range(1, len(list_of_integers) - 1):
-        if list_of_integers[list_index] == list_of_integers[list_index - 1] and list_of_integers[list_index] == list_of_integers[list_index + 1]:
+        if list_of_integers[list_index] == list_of_integers[list_index - 1] and
+                list_of_integers[list_index] == list_of_integers[list_index + 1]:
             return list_of_integers[list_index]
         elif list_of_integers[list_index] > list_of_integers[list_index - 1] and list_of_integers[list_index] > list_of_integers[list_index + 1]:
             return list_of_integers[list_index]
