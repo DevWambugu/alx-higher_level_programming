541519f6d4bcb3196f9e7d3958a1980341bbbf61
DevWambugu <venusdata100@gmail.com>
2023-09-30 08:53:35 +0000

    check whether the response is a valid json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+Python script that takes in a letter and
+sends a POST request to
+http://0.0.0.0:5000/search_user
+with the letter as a parameter
+'''
+import sys
+import requests
+
+if __name__ == "__main__":
+    q = sys.argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+
+    if q is None:
+        q = ""
+
+    data = {"q": q}
+    try:
+        response = requests.get(url, data=data)
+        response_json = response.json()
+
+        if response_json:
+            print(f"[{response_json['id']}] {response_json['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
